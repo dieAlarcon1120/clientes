822c922679a29b6fa98310b34866b7cac46f6e1e
diealarcon <die.alarcon1120@gmail.com>
2024-09-08 20:10:18 -0300

    probar commit

--- src/main/java/com/project/clients/ClientsApplication.java
+++ src/main/java/com/project/clients/ClientsApplication.java
@@ -2,7 +2,6 @@ package com.project.clients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 //probar commit
 
 @SpringBootApplication
